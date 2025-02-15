805cafe1fb7d1ead06013d837de4778920304b26
sadhiin <sanjidulislamsadhin1994@gmail.com>
2025-02-15 18:27:39 +0000

    Update the app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import csv
 import aiofiles
 import uvicorn
-from fastapi import FastAPI, Form, Request, Response, File, Depends,HTTPException, status
+from fastapi import FastAPI, Form, Request, Response, File, Depends, HTTPException, status
 from fastapi.responses import RedirectResponse
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
@@ -22,25 +22,24 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {'request': request})
 
 @app.post('/upload')
-async def upload(request:Request, pdf_file: bytes=File(), filename: str=Form(...)):
-    base_folder= 'static/docs/'
+async def upload(request: Request, pdf_file: bytes = File(), filename: str = Form(...)):
+    base_folder = 'static/docs/'
 
     if not os.path.isdir(base_folder):
         os.makedirs(base_folder)
     pdf_filename = os.path.join(base_folder, filename)
 
-    async with aiofiles.open(pdf_filename,'wb') as f:
+    async with aiofiles.open(pdf_filename, 'wb') as f:
         await f.write(pdf_file)
-
 
     response_data = jsonable_encoder({'msg': 'success', 'pdf_filename': pdf_filename})
 
     return JSONResponse(content=response_data)
 
 def get_csv(file_path):
-    question_list,answer_generator_chain = llm_pipeline(file_path)
+    question_list, answer_generator_chain = llm_pipeline(file_path)
 
-    base_folder = 'static/optput'
+    base_folder = 'static/output'
     if not os.path.isdir(base_folder):
         os.makedirs(base_folder)
 
@@ -54,7 +53,7 @@ def get_csv(file_path):
             print('Question: ', question)
             answer = answer_generator_chain.run(question)
             print('Answer: ', answer)
-            print('-'*50, end='')
+            print('-' * 50, end='')
             print('\n\n')
 
             csv_writer.writerow([question, answer])
@@ -62,13 +61,12 @@ def get_csv(file_path):
     return output_file
 
 @app.post('/analyze')
-async def chat(request: Request, pdf_filename: str=Form()):
-    output_file= get_csv(pdf_filename)
-    respons_data = jsonable_encoder(json.dump({'output_file': output_file}))
+async def chat(request: Request, pdf_filename: str = Form()):
+    output_file = get_csv(pdf_filename)
+    respons_data = jsonable_encoder(json.dumps({'output_file': output_file}))
 
     return Response(respons_data)
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     uvicorn.run('app:app', host='0.0.0.0', port=8080, reload=True)
